ad0f5a28d18cf29db095205f18d708d30363632b
Bayu Nurkholis <31134993+pinokio07@users.noreply.github.com>
2020-09-30 15:43:51 +0700

    Update halaman Service Darat

--- resources/views/pages/service/darat.blade.php
+++ resources/views/pages/service/darat.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 
 @section('header')
-<title>Arlamas | Tentang Kami</title>
+<title>Arlamas | Kargo Darat</title>
 @endsection
 
 @section('content')
@@ -27,7 +27,7 @@
 		<div class="card-group mb-4 row">			
 			<div class="card card-primary card-outline col-md-10 mr-3">
 				<div class="card-header">
-					<h3 class="card-title">Profil Perusahaan</h3>
+					<h3 class="card-title">Kargo Darat</h3>
 					<div class="card-tools">
 						<button type="button" class="btn btn-tool" data-card-widget="collapse">
 	            <i class="fas fa-minus"></i>
@@ -35,7 +35,154 @@
 					</div>
 				</div>
 				<div class="card-body">
-					
+					<div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-body">
+                  Kargo darat adalah Armada transportasi darat yang mengangkut dan mengirim barang-barang atau dokumen melalui jalur darat sesuai dengan jadwal, rute dan tujuan yang ditetapkan oleh perusahaan cargo.
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="card card-success card-outline">
+                <div class="card-header">
+                  <h3 class="card-title">BARANG KIRIMAN TRANSPORTASI DARAT</h3>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <p style="text-align: justify;"> Armada Laju Mas saat ini melayani kiriman untuk semua jenis barang, baik barang tidak bergerak maupun barang-barang bergerak seperti mobil dan motor, dan tidak menerima kiriman barang-barang terlarang sebagaimana yang tercantum dalam syarat dan ketentuan barang kiriman.</p>
+                  <p>Barang yang dapat Anda kirim via Armada Laju Mas seperti:</p>
+                  <ol type="1" style="margin-left: -22px;">
+                    <li>Perabot Pindahan Rumah Antar Kota/Provinsi.</li>
+                    <li>Perabot Pindahan Rumah / Kantor Ke Luar Negeri.</li>
+                    <li>Barang-barang Elektronik seperti HP, Laptop, Nootbook TV dll.</li>
+                    <li>Alat-alat Kesehatan Puskesmas / Rumah Sakit.</li>
+                    <li>Mesin percetakan dan mesin Foto Copy dan lainnya.</li>
+                    <li>Mesin-mesin Pertanian dan semua jenis mesin lainnya.</li>
+                    <li>Barang-barang Industri seperti Furniture, Keramik, Pipa, Besi dll.</li>
+                    <li>Barang-barang kiriman TKI Luar Negeri.</li>
+                  </ol>
+                  <p style="text-align: justify;">Bagi Anda yang membutuhkan layanan kirim barang, baik barang yang ringan maupun berat, Armada Laju Mas adalah solusi terbaik dan kami siap melayani Anda. Segera hubungi kami atau silahkan masukkan data kiriman di form order kirim atau Klik <a href="/pesan">disini</a>.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="card card-success card-outline">
+                <div class="card-header">
+                  <h3 class="card-title">LAYANAN KARGO & LOGISTIK ARMADA LAJU MAS</h3>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <p style="text-align: justify;">Pengalaman dan profesionalitas kami, mampu memberikan layanan pengiriman barang sesuai dengan kebutuhan dan pilihan user. Armada Laju Mas melayani kiriman seperti:</p>                  
+                  <ol type="1" style="margin-left: -22px;">
+                    <li>Cargo darat Door to Door.</li>
+                    <li>Cargo darat Port to Port.</li>
+                    <li>Cargo darat Door to Port.</li>                    
+                  </ol>
+                  <p style="text-align: justify;">Armada Laju Mas juga melayani Packing untuk barang-barang kiriman yang membutuhkan packing sebelum dikirim. Packing kami menggunakan bahan packing terbaru yang bermutu tinggi sesuai standar packing cargo darat, laut dan udara yang berlaku dan divisi packing kami melakukannya dengan rapi dan sangat hati-hati.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="card card-success card-outline">
+                <div class="card-header">
+                  <h3 class="card-title">ARMADA KARGO & LOGISTIK ARMADA LAJU MAS</h3>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <p style="text-align: justify;">Armada angkutan darat yang baik dengan berbagai jenis dan ukuran yang lengkap menjadi prioritas kami didalam memberikan layanan kargo dan logistik kepada para cutomer. Armada Laju Mas menyediakan Armada sbb;</p>
+                  <ol type="1" style="margin-left: -22px;">
+                    <li>Armada Truk Engkel baik (terbuka / box tertutup).</li>
+                    <li>Armada Truk CDD (Colt Double Diesel) box, bak dan losbak.</li>
+                    <li>Armada Tronton bak, box, dan flatbed.</li>   
+                    <li>Armada Truk Fuso bak dan box.</li>         
+                    <li>Armada Trailer.</li>
+                    <li>Armada Wingbox Tronton.</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="card card-success card-outline">
+                <div class="card-header">
+                  <h3 class="card-title">BIAYA KARGO DARAT ARMADA LAJU MAS</h3>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <p style="text-align: justify;">Prioritas layanan kargo Armada Laju Mas adalah kemanan, kecepatan kirim dan ketepatan sampai di alamat tujuan. Dengan pengalaman dan sistem yang profesional kami berkomitmen untuk meminimalkan budget biaya distribusi, sehingga saat ini harga kami sangat bersaing.</p>                  
+                </div>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="card card-success card-outline">
+                <div class="card-header">
+                  <h3 class="card-title">RUTE KARGO DARAT ARMADA LAJU MAS</h3>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <p style="text-align: justify;">Pulau Jawa :</p>
+                  <ol type="1" style="margin-left: -22px;">
+                    <li>JAWA BARAT : <br>
+                      Dari Jakarta ke Bekasi - Kerawang - Purwakarta - Bandung - Cimahi - Cianjur - Sukabumi - Bogor - Merak
+                    </li>
+                    <li>JATENG Rute Selatan : <br>
+                      Dari Jakarta ke Brebes - Cilacap - Kebumen - Purworejo - Bantul - Yogyakarta - Klaten - Solo
+                    </li>
+                    <li>JATENG Rute Tengah : <br>
+                      Dari Jakarta ke Tegal - Purwokerto - Banyumas - Banjarnegara - Wonosobo - Temanggung - Magelang
+                    </li>
+                    <li>JATENG Rute Utara : <br>
+                      Dari Jakarta ke Pekalongan - Kendal - Semarang - Jepara - Demak - Kudus - Blitar
+                    </li>
+                    <li>JAWA TIMUR : <br>
+                      Dari Jakarta ke Pekalongan - Kendal - Semarang - Jepara - Demak - Kudus - Blitar
+                    </li>
+                    <li>JAWA - BALI Cargo : <br>
+                      Dari Jakarta ke Surabaya - Madura - Denpasar (Bali)
+                    </li>
+                  </ol>
+                  <p style="text-align: justify;">Sumatera :</p>
+                  <ol type="1" style="margin-left: -22px;">
+                    <li>SUMATERA RUTE BARAT : Dari Jakarta ke Lampung - Bengkulu</li>
+                    <li>SUMATERA RUTE TIMUR : Dari Jakarta ke Lampung - Palembang - Jambi - Medan</li>
+                    <li>SUMATERA RUTE TENGAH : Dari Jakarta ke Lampung - Palembang - Jambi - Medan</li>                    
+                  </ol>
+                  <p style="text-align: justify;">Sulawesi :</p>
+                  <ol type="1" style="margin-left: -22px;">
+                    <li>SULAWESI RUTE BARAT : Dari Jakarta ke Lampung - Bengkulu</li>
+                    <li>SULAWESI RUTE TIMUR : Dari Jakarta ke Lampung - Palembang - Jambi - Medan</li>
+                    <li>SULAWESI RUTE TENGAH : Dari Jakarta ke Lampung - Palembang - Jambi - Medan</li>                    
+                  </ol>
+                  <p style="text-align: justify;">Kalimantan :</p>
+                  <ol type="1" style="margin-left: -22px;">
+                    <li>KALIMANTAN RUTE BARAT : Dari Jakarta ke Lampung - Bengkulu</li>
+                    <li>KALIMANTAN RUTE TIMUR : Dari Jakarta ke Lampung - Palembang - Jambi - Medan</li>
+                    <li>KALIMANTAN RUTE TENGAH : Dari Jakarta ke Lampung - Palembang - Jambi - Medan</li>                    
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
 				</div>
 			</div>
 			<div class="card card-primary card-outline col-md-2">
